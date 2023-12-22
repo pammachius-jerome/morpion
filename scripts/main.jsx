3b05ded1516645cd7a3e7ff1c7621ea54ff7e0be
[Pammachius Jérôme] <[scipions.prof@free.fr]>
2023-12-22 11:29:44 +0100

    tuto react fini

--- scripts/main.jsx
+++ scripts/main.jsx
@@ -54,8 +54,6 @@ class Game extends React.Component {
             xIsNext: true,
         };
     }
-
-
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
